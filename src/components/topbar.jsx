d47d6063028316b07e6dc3bea7dd2aa35dd7271d
joao-montanari <joao.unipbr@gmail.com>
2023-04-14 09:44:30 -0300

    Desenvolvimento da pagina shopping

---
+++ src/components/topbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet, Text, Image, TextInput, TouchableOpacity } from "react-native";
+
+export default function TopBar() {
+    return (
+        <View style={style.container}>
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: '0.1',
+        backgroundColor: '#181A20',
+    }
+})
--- src/pages/shopping.jsx
+++ src/pages/shopping.jsx
@@ -3,6 +3,8 @@ import { View, StyleSheet, Text, Image, TextInput, TouchableOpacity } from "reac
 import { useNavigation } from '@react-navigation/native';
 import { useState } from "react";
 
+import TopBar from "../components/topbar";
+
 export default function Shopping() {
     const navigate = useNavigation();
     
@@ -10,9 +12,7 @@ export default function Shopping() {
     
     return (
         <View style={styles.main}>
-            <View style={styles.child}>
-
-            </View>
+            <TopBar/>
             <View style={styles.container}>
                 <TextInput
                     style={styles.input}
@@ -21,15 +21,101 @@ export default function Shopping() {
                     placeholder="O que você está procurando?"
                 />
                 <View style={styles.box}>
-                    <Text style={{  }}>
-                        Categorias
+                    <Text style={{ color: 'white', fontWeight: 'bold', fontSize: '25px', marginBottom: '15px' }}>
+                        Categorias:
                     </Text>
-                    <Viwe style={styles.grade}>
-
-                    </Viwe>
-                    <Viwe style={styles.grade}>
-
-                    </Viwe>
+                    <View style={styles.child}>
+                        <TouchableOpacity 
+                            style={styles.btn}
+                        >
+                            <Image
+                                source = {require('../../public/icons/legume.svg')}
+                                style = {{ width: '35px', height: '35px', marginTop: '5px' }}
+                            />
+                            <Text style={{ color: '#BCBCBC', fontSize: '12px', marginTop: '2px' }}>
+                                Legumes
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                            style={styles.btn}
+                        >
+                            <Image
+                                source = {require('../../public/icons/salada.svg')}
+                                style = {{ width: '35px', height: '35px', marginTop: '5px' }}
+                            />
+                            <Text style={{ color: '#BCBCBC', fontSize: '12px', marginTop: '2px' }}>
+                                Saladas
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                            style={styles.btn}
+                        >
+                            <Image
+                                source = {require('../../public/icons/fruta.svg')}
+                                style = {{ width: '35px', height: '35px', marginTop: '5px' }}
+                            />
+                            <Text style={{ color: '#BCBCBC', fontSize: '12px', marginTop: '2px' }}>
+                                Frutas
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                            style={styles.btn}
+                        >
+                            <Image
+                                source = {require('../../public/icons/molho.svg')}
+                                style = {{ width: '35px', height: '35px', marginTop: '5px' }}
+                            />
+                            <Text style={{ color: '#BCBCBC', fontSize: '12px', marginTop: '2px' }}>
+                                Molhos
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.child}>
+                        <TouchableOpacity 
+                            style={styles.btn}
+                        >
+                            <Image
+                                source = {require('../../public/icons/peixe.svg')}
+                                style = {{ width: '35px', height: '35px', marginTop: '5px' }}
+                            />
+                            <Text style={{ color: '#BCBCBC', fontSize: '12px', marginTop: '2px' }}>
+                                Peixes
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                            style={styles.btn}
+                        >
+                            <Image
+                                source = {require('../../public/icons/agricultura.svg')}
+                                style = {{ width: '35px', height: '35px', marginTop: '5px' }}
+                            />
+                            <Text style={{ color: '#BCBCBC', fontSize: '12px', marginTop: '2px' }}>
+                                cultivo
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                            style={styles.btn}
+                        >
+                            <Image
+                                source = {require('../../public/icons/planta.svg')}
+                                style = {{ width: '35px', height: '35px', marginTop: '5px' }}
+                            />
+                            <Text style={{ color: '#BCBCBC', fontSize: '12px', marginTop: '2px' }}>
+                                Plantas
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                            style={styles.btn}
+                        >
+                            <Image
+                                source = {require('../../public/icons/cozinha.svg')}
+                                style = {{ width: '35px', height: '35px', marginTop: '5px' }}
+                            />
+                            <Text style={{ color: '#BCBCBC', fontSize: '12px', marginTop: '2px' }}>
+                                Cozinha
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         </View>
@@ -54,5 +140,22 @@ const styles = StyleSheet.create({
         paddingLeft: '16px',
         fontSize: '17px',
         margin: 'auto',
+    },
+    box: {
+        width: '85%',
+        margin: 'auto',
+    },
+    child: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: '18px',
+    },
+    btn: {
+        width: '75px',
+        height: '65px',
+        borderRadius: '15px',
+        alignItems: 'center',
+        backgroundColor: '#3A3A3A',
     },
 })
